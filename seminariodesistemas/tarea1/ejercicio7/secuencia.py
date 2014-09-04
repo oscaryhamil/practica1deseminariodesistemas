08fe09476e4f8346eaefdc09a34037c388e4387e
Oscar <oscarcaba@hotmail.com>
2014-09-04 19:19:27 -0400

    es el ejercicio 7

---
+++ seminariodesistemas/tarea1/ejercicio7/secuencia.py
@@ -0,0 +1,21 @@
+a =  input("digite un numero: ")
+x = 1
+y = 1
+o = 0
+while x <= a:
+	y = 1
+	o = x
+	while y <= x:
+		print o,
+		y+=1
+	print
+	x+=1
+x = x -1
+while x >= 1:
+	y = 1
+	o = x
+	while y <= x:
+		print o,
+		y+=1
+	print
+	x-=1
